54772620b04e73182420f4fb729eba0547337be3
xiaoliang <32414154@qq.com>
2015-04-24 01:09:54 +0800

    fix newer less file checking bug

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,13 +43,59 @@ module.exports = function (grunt) {
         },
         newer: {
             options: {
-                override: function (details, include) {
-                    if (details.task === 'less') {
-                        includeIfImportedFileChanged(details.path, details.time, include);
-                    } else {
-                        include(true);
+                override: (function () {
+                    var fs = require('fs');
+                    var path = require('path');
+
+                    function includeIfImportedFileChanged(lessFile, mTime, include) {
+                        function fileChanged(file) {
+                            return fs.existsSync(file) && fs.statSync(file).mtime > mTime;
+                        }
+
+                        function whenFileChanged(file, include, process) {
+                            fs.readFile(file, "utf8", function (err, data) {
+                                var lessDir = path.dirname(file),
+                                    regex = /@import\s*(?:\(.*?\))?\s*"(.*?)(?:\.less)?"\s*;/g,
+                                    result, imports = [];
+                                while (result = regex.exec(data)) {
+                                    // All of my less files are in the same directory,
+                                    // other paths may need to be traversed for different setups...
+                                    var baseName = result[1];
+                                    var importFile = lessDir + '/' + baseName + '.less';
+                                    imports.push(importFile);
+                                    if (fileChanged(importFile)) {
+                                        include(true);
+                                        return;
+                                    }
+                                }
+                                process(imports);
+                            });
+                        }
+
+                        var files = [];
+                        var processed = {lessFile: true};
+                        whenFileChanged(lessFile, include, function (imports) {
+                            files = files.concat(imports);
+                            while (files.length) {
+                                var filename = files.pop();
+                                if (!processed[filename]) {
+                                    processed[filename] = true;
+                                    whenFileChanged(filename, include, arguments.callee);
+                                    return;
+                                }
+                            }
+                            include(false);
+                        });
                     }
-                }
+
+                    return function (details, include) {
+                        if (details.task === 'less') {
+                            includeIfImportedFileChanged(details.path, details.time, include);
+                        } else {
+                            include(true);
+                        }
+                    }
+                })()
             }
         },
         cssmin: {
